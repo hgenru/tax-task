40e824d337c8867caadbc6431720d57d2b59688d
Alexandr Plesovskikh <me@hgen.ru>
2015-03-20 23:07:46 +0500

    New server settings

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express'),
     fs = require('fs')
     url = require('url');
 
+var http = require("http");
+
 var app = express();
 
 app.use('/', express.static(__dirname + '/'));  
@@ -21,6 +23,44 @@ app.post('/receive', function(request, respond) {
     });
 });
 
-console.log('Starting server at: ' + process.env.PORT || 5000);
+var port = normalizePort(process.env.PORT || '8081');
+var server = http.createServer(app);
+server.on('error', onError);
+server.on('listening', onListening);
+server.listen(port);
 
-app.listen(process.env.PORT || 5000);
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if(isNaN(port)) {
+        return val;
+    }
+    if(port >= 0) {
+        return port;
+    }
+    return false;
+}
+
+function onError(error) {
+    if(error.syscall !== 'listen') {
+        throw error;
+    }
+    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+    switch(error.code) {
+    case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+    case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+    default:
+        throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    console.log('Listening on localhost:' + addr.port);
+}
