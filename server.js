c06878eccde4a1b30ba1e146a312b0b231236509
Alexandr Plesovskikh <me@hgen.ru>
2015-03-20 23:46:24 +0500

    * more logs

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ function onError(error) {
 function onListening() {
     var addr = server.address();
     var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    console.log('Listening on localhost:' + addr.port);
+    console.log('Listening on ' + server_ip_address + ':' + addr.port);
 }
